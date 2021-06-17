70be5528076eae747150dca9ed02d46de890a8f3
Sebastien Noel <snoel45@gmail.com>
2021-06-16 21:33:08 -0400

    1

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const { models: { Artist }} = require('./db');
+const path = require("path");
+const {
+  models: { Artist },
+} = require("./db");
 
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
-
-app.get('/api/artists', async(req, res, next)=> {
+app.get("/api/artists", async (req, res, next) => {
   try {
-    res.send(await Artist.findAll({
-      order: [['name']]
-    }));
-  }
-  catch(ex){
+    res.send(
+      await Artist.findAll({
+        order: [["name"]],
+      })
+    );
+  } catch (ex) {
     next(ex);
   }
 });
+
+app.get();
 
 module.exports = app;
--- db/index.js
+++ db/index.js
@@ -1,37 +1,112 @@
-const Sequelize = require('sequelize');
-const { STRING, UUID, UUIDV4 } = Sequelize.DataTypes;
+const Sequelize = require("sequelize");
+const { STRING, UUID, UUIDV4, INTEGER } = Sequelize.DataTypes;
 
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_tdd');
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_tdd"
+);
 
-const Artist = conn.define('artist', {
+const Artist = conn.define("artist", {
   id: {
     type: UUID,
     defaultValue: UUIDV4,
-    primaryKey: true
+    primaryKey: true,
   },
-  name: STRING
+  name: STRING,
 });
 
-const syncAndSeed = async()=> {
+const Album = conn.define("album", {
+  name: STRING,
+});
+
+const Song = conn.define("song", {
+  name: STRING,
+  duration: STRING,
+});
+
+const Track = conn.define("track", {
+  idx: INTEGER,
+  name: STRING,
+});
+
+Artist.hasMany(Album);
+Artist.hasMany(Song);
+
+Song.belongsTo(Artist);
+Album.belongsTo(Artist);
+
+Song.hasMany(Track);
+Album.hasMany(Track);
+
+Track.belongsTo(Song);
+Track.belongsTo(Album);
+
+const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const [weeknd, metalica, adele] = await Promise.all([
-    Artist.create({ name: 'The Weeknd'}),
-    Artist.create({ name: 'Metalica'}),
-    Artist.create({ name: 'Adele'})
-  ]); 
+    Artist.create({ name: "The Weeknd" }),
+    Artist.create({ name: "Metalica" }),
+    Artist.create({ name: "Adele" }),
+  ]);
+
+  const [starboy, nineteen, metallica] = await Promise.all([
+    Album.create({ name: "Starboy" }),
+    Album.create({ name: "Nineteen" }),
+    Album.create({ name: "Metallica" }),
+  ]);
+
+  const secrets = await Song.create({
+    name: "Secrets",
+    duration: "2:00",
+    artistid: weeknd.id,
+  });
+
+  const sandman = await Song.create({
+    name: "Sandman",
+    duration: "2:00",
+    artistId: metalica.id,
+  });
+  const daydreamer = await Song.create({
+    name: "Daydreamer",
+    duration: "2:00",
+    artistId: adele.id,
+  });
+
+  const secretsTrack = await Track.create({
+    name: secrets.name,
+    idx: 1,
+    albumId: starboy.id,
+    songId: secrets.id,
+  });
+
+  const sandmanTrack = await Track.create({
+    name: sandman.name,
+    idx: 1,
+    albumId: metallica.id,
+    songId: sandman.id,
+  });
+
+  const daydreamerTrack = await Track.create({
+    name: daydreamer.name,
+    idx: 1,
+    albumId: nineteen.id,
+    songId: daydreamer.id,
+  });
+
   return {
     artists: {
       weeknd,
-      metalica
-    }
+      metalica,
+      adele,
+    },
   };
-
 };
-
 
 module.exports = {
   syncAndSeed,
   models: {
-    Artist
-  }
+    Artist,
+    Album,
+    Song,
+    Track,
+  },
 };
